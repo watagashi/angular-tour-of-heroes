cbcab6cf3c21d0d8167b11145efc3c7257ea6fc8
WADA Takashi <twada@amy.hi-ho.ne.jp>
2019-01-01 23:56:34 +0900

    Add hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -68,4 +68,12 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+
+  /** POST: サーバーに新しいヒーローを登録する */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((addedHero: Hero) => this.log(`added hero w/ id=${addedHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
